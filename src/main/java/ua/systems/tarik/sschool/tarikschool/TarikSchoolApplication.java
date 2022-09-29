2f19baade90eb9cae9d9139d898c0b5b068e2b39
lubchenkodenys <denyslubchenko@gmail.com>
2022-09-29 22:29:37 +0300

    Comment pupa

--- src/main/java/ua/systems/tarik/sschool/tarikschool/TarikSchoolApplication.java
+++ src/main/java/ua/systems/tarik/sschool/tarikschool/TarikSchoolApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TarikSchoolApplication {
-	//pupa
+	//pupa!
 	public static void main(String[] args) {
 		SpringApplication.run(TarikSchoolApplication.class, args);
 	}
