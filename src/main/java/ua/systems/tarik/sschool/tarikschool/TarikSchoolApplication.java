ebd8af8e2d7ff447709329d70da2765496b8e8d6
lisak <lisk.semen@gmail.com>
2022-09-29 17:06:32 +0200

    Initial commit

---
+++ src/main/java/ua/systems/tarik/sschool/tarikschool/TarikSchoolApplication.java
@@ -0,0 +1,13 @@
+package ua.systems.tarik.sschool.tarikschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarikSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarikSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/systems/tarik/sschool/tarikschool/TarikSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.systems.tarik.sschool.tarikschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarikSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
