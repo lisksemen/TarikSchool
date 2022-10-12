53a7221405b5afd08b270e7a05b50e8c01d88f6a
lisak <lisk.semen@gmail.com>
2022-10-12 22:13:21 +0200

    Added controller, navbar. Code was formatted

--- src/test/java/ua/systems/tarik/sschool/tarikschool/TarikSchoolApplicationTests.java
+++ src/test/java/ua/systems/tarik/sschool/tarikschool/TarikSchoolApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TarikSchoolApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
