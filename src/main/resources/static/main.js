0114e6ae1d81221411d1550a5645cb8bb0926582
lisak <lisk.semen@gmail.com>
2022-10-12 22:43:32 +0200

    Fixed navbar font and color. Reformatted js and css code as well

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,27 +1,26 @@
-
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
 });
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
-function showP(){
-    document.getElementById("formContact").style.display ="none";
+function showP() {
+    document.getElementById("formContact").style.display = "none";
     document.getElementById("btnP").style.display = "none";
     document.getElementById("formFeedback").style.display = "block";
     document.getElementById("btnQ").style.display = "block";
 }
-function showQ()
-{
-    document.getElementById("formFeedback").style.display ="none";
+
+function showQ() {
+    document.getElementById("formFeedback").style.display = "none";
     document.getElementById("btnQ").style.display = "none";
     document.getElementById("formContact").style.display = "block";
     document.getElementById("btnP").style.display = "block";
