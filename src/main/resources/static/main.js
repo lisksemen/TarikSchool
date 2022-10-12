5e5318c63cabf22b11bc91c6b2558c3055e7c5d7
lagysha <nikita.volam@gmail.com>
2022-10-12 16:24:11 +0300

    Added footer

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,13 @@
+function showP(){
+    document.getElementById("formContact").style.display ="none";
+    document.getElementById("btnP").style.display = "none";
+    document.getElementById("formFeedback").style.display = "block";
+    document.getElementById("btnQ").style.display = "block";
+}
+function showQ()
+{
+    document.getElementById("formFeedback").style.display ="none";
+    document.getElementById("btnQ").style.display = "none";
+    document.getElementById("formContact").style.display = "block";
+    document.getElementById("btnP").style.display = "block";
+}
