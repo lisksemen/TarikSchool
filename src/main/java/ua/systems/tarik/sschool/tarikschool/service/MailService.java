e6f69bd088c57de8922ef16472dd50b1c16a6f7a
lisak <lisk.semen@gmail.com>
2022-10-13 15:34:42 +0200

    Implemented form data sending via gmail, added dependency to pom.xml

---
+++ src/main/java/ua/systems/tarik/sschool/tarikschool/service/MailService.java
@@ -0,0 +1,36 @@
+package ua.systems.tarik.sschool.tarikschool.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class MailService {
+    private JavaMailSender mailSender;
+
+    private SimpleMailMessage mailMessage;
+
+    public void sendFeedbackMail (String email, String phone, String name, String message) {
+        mailMessage.setSubject("Feedback form submission");
+        mailMessage.setText("Form has submitted recently!\n" +
+                "Name: " + name + "\n" +
+                "Email: " + email + "\n" +
+                "Phone: " + phone + "\n" +
+                "Message:\n'''\n" + message + "\n'''"
+        );
+
+        mailSender.send(mailMessage);
+    }
+
+    public void sendCallbackMail (String name, String phone) {
+        mailMessage.setSubject("Callback form submission");
+        mailMessage.setText("Form has submitted recently!\n" +
+                "Name: " + name + "\n" +
+                "Phone: " + phone + "\n"
+        );
+
+        mailSender.send(mailMessage);
+    }
+}
