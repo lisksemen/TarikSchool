92b72976415820784c8bb8183cc314c2c40c2c11
lisak <lisk.semen@gmail.com>
2022-11-08 14:30:21 +0100

    Implemented captcha validation to forms

---
+++ src/main/java/ua/systems/tarik/sschool/tarikschool/model/GoogleResponse.java
@@ -0,0 +1,64 @@
+package ua.systems.tarik.sschool.tarikschool.model;
+
+import com.fasterxml.jackson.annotation.*;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonPropertyOrder({
+        "success",
+        "challenge_ts",
+        "hostname",
+        "error-codes"
+})
+@Data
+public class GoogleResponse {
+
+    @JsonProperty("success")
+    private boolean success;
+
+    @JsonProperty("challenge_ts")
+    private String challengeTs;
+
+    @JsonProperty("hostname")
+    private String hostname;
+
+    @JsonProperty("error-codes")
+    private ErrorCode[] errorCodes;
+
+    @JsonIgnore
+    public boolean hasClientError() {
+        ErrorCode[] errors = ErrorCode.values();
+        for(ErrorCode error : errors) {
+            switch(error) {
+                case InvalidResponse:
+                case MissingResponse:
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    enum ErrorCode {
+        MissingSecret,     InvalidSecret,
+        MissingResponse,   InvalidResponse;
+
+        private static final Map<String, ErrorCode> errorsMap = new HashMap<String, ErrorCode>(4);
+
+        static {
+            errorsMap.put("missing-input-secret",   MissingSecret);
+            errorsMap.put("invalid-input-secret",   InvalidSecret);
+            errorsMap.put("missing-input-response", MissingResponse);
+            errorsMap.put("invalid-input-response", InvalidResponse);
+        }
+
+        @JsonCreator
+        public static ErrorCode forValue(String value) {
+            return errorsMap.get(value.toLowerCase());
+        }
+    }
+
+}
---
+++ src/main/java/ua/systems/tarik/sschool/tarikschool/service/CaptchaService.java
@@ -0,0 +1,43 @@
+package ua.systems.tarik.sschool.tarikschool.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestOperations;
+import ua.systems.tarik.sschool.tarikschool.config.CaptchaConfig;
+import ua.systems.tarik.sschool.tarikschool.model.GoogleResponse;
+
+import java.net.URI;
+import java.util.regex.Pattern;
+
+@Component
+@AllArgsConstructor
+public class CaptchaService {
+
+    private CaptchaConfig captchaSettings;
+
+    private RestOperations restTemplate;
+
+    private static final Pattern RESPONSE_PATTERN = Pattern.compile("[A-Za-z0-9_-]+");
+
+    public void processResponse(String response, String IP) {
+        if(!responseSanityCheck(response)) {
+            throw new RuntimeException("Response contains invalid characters");
+        }
+
+        URI verifyUri = URI.create(String.format(
+                "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s&remoteip=%s",
+                captchaSettings.getSecret(), response, IP));
+
+        GoogleResponse googleResponse = restTemplate.getForObject(verifyUri, GoogleResponse.class);
+
+
+        if(googleResponse == null || !googleResponse.isSuccess()) {
+            throw new RuntimeException("reCaptcha was not successfully validated");
+        }
+    }
+
+    private boolean responseSanityCheck(String response) {
+        return StringUtils.hasLength(response) && RESPONSE_PATTERN.matcher(response).matches();
+    }
+}
